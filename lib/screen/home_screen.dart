2ef6364be0d4ec81fe4d2b9f03752db30479b9c3
hubgood98 <heej1998@gmail.com>
2024-09-05 01:52:50 +0900

    feat: 웹뷰로 작동하도록 수정

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class HomeScreen extends StatelessWidget{
+  WebViewController webViewController = WebViewController()
 
-  const HomeScreen({Key? key}) : super(key: key);
+  //WebViewController의 loadRequest() 함수를 실행
+  ..loadRequest(Uri.parse('https://velog.io/@hubgood98/posts'))
+
+    //자바 스크립트가 제한 없이 실행될 수 있도록 설정 <=> 반대기능은 restricted
+  ..setJavaScriptMode(JavaScriptMode.unrestricted);
+
+  HomeScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('메인 화면'),
+
+      appBar: AppBar(
+        backgroundColor: Colors.orange,
+        title: Text('S 라인 블로그'),
+        centerTitle: true,
+      ),
+
+      body: WebViewWidget(
+          controller: webViewController,
+      ),
     );
   }
 
